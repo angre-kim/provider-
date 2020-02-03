082731105cb3bc04c604d944d7c74c7b0d5633dd
angre-kim <angre@daum.net>
2020-02-03 15:27:21 +0900

    initial

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final String data = 'top screet';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+              title: Text(data),
+          ),
+          body: Level1(data),
+        )
+    );
+  }
+}
+
+class Level1 extends StatelessWidget {
+  final String data;
+  Level1(this.data);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Level2(data),
+    );
+  }
+}
+
+class Level2 extends StatelessWidget {
+  final String data;
+  Level2(this.data);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children:[
+        Container(),
+        Level3(),
+
+      ],
+    );
+  }
+}
+
+class Level3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Need data');
+  }
+}
+
